3519668c6ed1dc7b6082a1d5192bc9f068d58037
Sebastian Ene <ene.sebastian@gmail.com>
2023-07-01 19:49:38 +0300

    set verbose=0 for moves, current_qs_list and future_qs_list to be able to see the print on the console
    added nr_discov_tiles in the for
    
    updated the self.model.fit() function with callbacks= None to avoid some compile error

--- DQN/DQN_agent.py
+++ DQN/DQN_agent.py
@@ -7,10 +7,13 @@ import warnings
 warnings.filterwarnings('ignore')
 
 from collections import deque
-from minesweeper_env import *
+#from minesweeper_env import *
+import minesweeper_env
 # use my_tensorboard2.py if using tensorflow v2+, use my_tensorboard.py otherwise
 from my_tensorboard2 import *
 from DQN import *
+import numpy as np
+import random
 
 # Environment settings
 MEM_SIZE = 50_000 # number of moves to store in replay buffer
@@ -67,7 +70,7 @@ class DQNAgent(object):
         if rand < self.epsilon: # random move (explore)
             move = np.random.choice(unsolved)
         else:
-            moves = self.model.predict(np.reshape(state, (1, self.env.nrows, self.env.ncols, 1)))
+            moves = self.model.predict(np.reshape(state, (1, self.env.nrows, self.env.ncols, 1)), verbose = 0)
             moves[board!=-0.125] = np.min(moves) # set already clicked tiles to min value
             move = np.argmax(moves)
 
@@ -83,14 +86,15 @@ class DQNAgent(object):
         batch = random.sample(self.replay_memory, BATCH_SIZE)
 
         current_states = np.array([transition[0] for transition in batch])
-        current_qs_list = self.model.predict(current_states)
+        current_qs_list = self.model.predict(current_states, verbose = 0)
 
         new_current_states = np.array([transition[3] for transition in batch])
-        future_qs_list = self.target_model.predict(new_current_states)
+        future_qs_list = self.target_model.predict(new_current_states, verbose = 0)
 
         X,y = [], []
 
-        for i, (current_state, action, reward, new_current_state, done) in enumerate(batch):
+#        for i, (current_state, action, reward, new_current_state, done) in enumerate(batch):
+        for i, (current_state, action, reward, new_state, nr_discov_tiles, done) in enumerate(batch):
             if not done:
                 max_future_q = np.max(future_qs_list[i])
                 new_q = reward + DISCOUNT * max_future_q
@@ -103,9 +107,12 @@ class DQNAgent(object):
             X.append(current_state)
             y.append(current_qs)
 
+        # self.model.fit(np.array(X), np.array(y), batch_size=BATCH_SIZE,
+        #                shuffle=False, verbose=0, callbacks=[self.tensorboard]\
+        #                if done else None)
+
         self.model.fit(np.array(X), np.array(y), batch_size=BATCH_SIZE,
-                       shuffle=False, verbose=0, callbacks=[self.tensorboard]\
-                       if done else None)
+                       shuffle=False, verbose=0, callbacks= None)
 
         # updating to determine if we want to update target_model yet
         if done:
